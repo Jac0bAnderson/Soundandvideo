9e50d60a33a9989714a4e385349230d8f0c5ea10
Jac0bAnderson <jake912235@gmail.com>
2015-11-19 19:17:07 -0700

    can now play video

--- app/src/main/java/com/example/jand6944/soundandvideo/VideoActivity.java
+++ app/src/main/java/com/example/jand6944/soundandvideo/VideoActivity.java
@@ -7,18 +7,34 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+import android.net.Uri;
+import android.widget.VideoView;
+import android.widget.MediaController;
 
 public class VideoActivity extends AppCompatActivity {
 private Button videoHome;
+    private Uri videoLocation;
+    private VideoView videoView;
+    private MediaController videoDirector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video);
 
+
+        videoView =(VideoView) findViewById(R.id.videoView);
         videoHome =(Button) findViewById(R.id.videoHome);
 
+        videoLocation = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.badrats);
+        videoDirector = new MediaController(this);
+        setupMedia();
         setupListeners();
+    }
+   private void setupMedia()
+    {
+        videoView.setMediaController(videoDirector);
+        videoView.setVideoURI(videoLocation);
     }
 
     @Override
